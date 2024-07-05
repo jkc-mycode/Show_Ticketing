18124cf698a330bcb3334c329fee9116fb473907
jkc-mycode <jkc9709@naver.com>
2024-07-05 12:04:36 +0900

    Add/find user tickets api

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -5,6 +5,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { Ticket } from 'src/seat/entities/ticket.entity';
 
 @Module({
   imports: [
@@ -15,7 +16,7 @@ import { User } from './entities/user.entity';
       }),
       inject: [ConfigService],
     }),
-    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([User, Ticket]),
   ],
   providers: [UserService],
   controllers: [UserController],
