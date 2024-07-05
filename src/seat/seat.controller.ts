bc1daba0dac174fc11571d9cd63da07e091a7f5a
jkc-mycode <jkc9709@naver.com>
2024-07-05 14:31:34 +0900

    Add/ticket cancel api

--- src/seat/seat.controller.ts
+++ src/seat/seat.controller.ts
@@ -4,10 +4,18 @@ import { AuthGuard } from '@nestjs/passport';
 import { UserInfo } from 'src/utils/userInfo.decorator';
 import { User } from 'src/user/entities/user.entity';
 import { ReserveSeatDto } from './dto/reserve-seat.dto';
+import { CancelTicketDto } from './dto/cancel-ticket.dto';
 
 @Controller('seats')
 export class SeatController {
   constructor(private readonly seatService: SeatService) {}
+
+  // 공연 예매 취소
+  @UseGuards(AuthGuard('jwt'))
+  @Post('/cancel')
+  async cancelTicket(@UserInfo() user: User, @Body() cancelTicketDto: CancelTicketDto) {
+    return await this.seatService.cancelTicket(user, cancelTicketDto);
+  }
 
   // 공연 좌석 예매
   @UseGuards(AuthGuard('jwt'))
--- src/seat/seat.service.ts
+++ src/seat/seat.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Seat } from './entities/seat.entity';
 import { DataSource, Repository } from 'typeorm';
@@ -10,6 +15,7 @@ import _ from 'lodash';
 import { Ticket } from './entities/ticket.entity';
 import { Grade } from './types/grade.type';
 import { ShowTime } from 'src/show/entities/showTime.entity';
+import { CancelTicketDto } from './dto/cancel-ticket.dto';
 
 @Injectable()
 export class SeatService {
@@ -111,6 +117,7 @@ export class SeatService {
     } catch (err) {
       // 트랜젝션 실패 시 롤백
       await queryRunner.rollbackTransaction();
+      throw new InternalServerErrorException('좌석 예매에 실패했습니다.');
     } finally {
       await queryRunner.release();
     }
@@ -140,5 +147,79 @@ export class SeatService {
       };
     });
     return seatInfo;
+  }
+
+  // 공연 예매 취소
+  async cancelTicket(user: User, cancelTicketDto: CancelTicketDto) {
+    const { ticketId } = cancelTicketDto;
+
+    // 해당 티켓이 존재하는지 확인
+    const ticket = await this.ticketRepository.findOne({
+      where: { id: ticketId },
+    });
+    if (_.isNil(ticket)) {
+      throw new NotFoundException('예매된 티켓이 없습니다.');
+    }
+
+    // 티켓의 소유자와 로그인한 사용자가 같은지 확인
+    if (ticket.userId !== user.id) {
+      throw new UnauthorizedException('접근 권한이 없습니다.');
+    }
+
+    // 해당 좌석 조회
+    const seat = await this.seatRepository.findOne({
+      where: { id: ticket.seatId },
+    });
+
+    // 해당 공연의 좌석 수 조회
+    const place = await this.showPlaceRepository.findOne({
+      where: { showTimeId: seat.showTimeId },
+    });
+
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      // 사용자 포인트 환불
+      const updatedUser = this.userRepository.merge(user, { point: user.point + ticket.price });
+
+      // 해당 좌석 예매 상태 변경
+      const updatedSeat = this.seatRepository.merge(seat, { isReserved: false });
+
+      // 어떤 좌석의 수를 변경할지에 대한 객체 생성
+      const condition = {};
+      if (seat.grade === Grade.A) {
+        condition['seatA'] = place.seatA + 1;
+      } else if (seat.grade === Grade.S) {
+        condition['seatS'] = place.seatS + 1;
+      } else if (seat.grade === Grade.R) {
+        condition['seatR'] = place.seatR + 1;
+      } else {
+        condition['seatVip'] = place.seatVip + 1;
+      }
+
+      // 잔여 좌석 수 변경
+      const updatedPlace = this.showPlaceRepository.merge(place, condition);
+
+      // 공연 예매 취소 (티켓 isCanceled: True)
+      const canceledTicket = this.ticketRepository.merge(ticket, { isCanceled: true });
+
+      await Promise.all([
+        queryRunner.manager.save(updatedUser),
+        queryRunner.manager.save(updatedSeat),
+        queryRunner.manager.save(updatedPlace),
+        queryRunner.manager.save(canceledTicket),
+      ]);
+
+      await queryRunner.commitTransaction();
+
+      return { message: '공연 예매 취소에 성공했습니다.' };
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      throw new InternalServerErrorException('공연 예매 취소에 실패했습니다.');
+    } finally {
+      await queryRunner.release();
+    }
   }
 }
